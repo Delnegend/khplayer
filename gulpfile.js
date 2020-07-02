9e586163abeabf4eca8d4080ec0e5e39135e52f8
dngnd-storage <meovuinhon@gmail.com>
2020-07-02 21:43:12 +0700

    remove gulp-sass, browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,109 +2,28 @@
 // jshint esversion: 10 
 'use strict';
 
-// General
 const {
   parallel,
   src,
   dest,
   series,
   watch,
-  gulp
+  // gulp
 } = require('gulp'),
   rename = require("gulp-rename"),
   sourcemaps = require('gulp-sourcemaps'),
   minify = require('gulp-minify'),
-  babel = require("gulp-babel"),
   concat = require('gulp-concat'),
-  autoprefixer = require('gulp-autoprefixer'),
-  sass = require("gulp-sass"),
-  minifyHTML = require("gulp-htmlmin"),
-  browserSync = require('browser-sync').create();
-sass.compiler = require("node-sass");
+  minifyHTML = require("gulp-htmlmin");
 
-const khp_gulp = {
-  // Điều chỉnh, thêm bớt dữ liệu tại đây
+var khplayerGulp = {
   path: {
-    sass: "./src/khplayer.scss",
-    js: "./src/khplayer.js",
-    dist: "./dist/"
+    js: "src/khplayer.js"
   },
-  name: {
-    css: "khplayer.css",
-    js: "khplayer.js",
-  },
-  // Phần dưới này tốt nhất mặc kệ
-  export: {
-    css() {
-      return src(khp_gulp.path.sass)
-        .pipe(rename(khp_gulp.name.css))
-        .pipe(rename(function (path) {
-          return {
-            dirname: path.dirname + "/",
-            basename: path.basename + ".min",
-            extname: ".css",
-          };
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(sass.sync({
-          "outputStyle": "compressed",
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-          cascade: true
-        }))
-        .pipe(sourcemaps.write("./"))
-        .pipe(dest(khp_gulp.path.dist))
-        .pipe(browserSync.stream());
-    },
-    js: {
-      standard() {
-        return src(khp_gulp.path.js)
-          .pipe(concat(khp_gulp.name.js))
-          .pipe(sourcemaps.init())
-          .pipe(minify({
-            mangle: false,
-            noSource: true,
-            ext: {
-              min: ".min.js"
-            }
-          }))
-          .pipe(sourcemaps.write("./"))
-          .pipe(dest(khp_gulp.path.dist))
-          .pipe(browserSync.stream());
-
-      },
-    }
-  },
-  watch() {
-    browserSync.init({
-      server: {
-        baseDir: "./"
-      }
-    });
-    watch([khp_gulp.path.sass, khp_gulp.path.js, "./demo/index.html"], exports.build, function () {
-      browserSync.reload();
-    });
-  },
-};
-
-var genGulp = {
-  // Điều chỉnh, thêm bớt dữ liệu tại đây
-  path: {
-    html: "genScreen/src/content.html",
-    sass: "genScreen/src/style.scss",
-    js: "genScreen/src/script.js",
-    dist: "genScreen/dist"
-  },
-  name: {
-    html: "index.html",
-    css: "vendor.css",
-    js: "vendor.js"
-  },
-  // Phần dưới này tốt nhất mặc kệ
   export: {
     js() {
-      return src(genGulp.path.js)
-        .pipe(concat(genGulp.name.js))
+      return src(khplayerGulp.path.js)
+        .pipe(rename("khplayer.js"))
         .pipe(sourcemaps.init())
         .pipe(minify({
           mangle: false,
@@ -114,39 +33,66 @@ var genGulp = {
           }
         }))
         .pipe(sourcemaps.write("./"))
-        .pipe(dest(genGulp.path.dist))
-        .pipe(browserSync.stream());
-    },
-    css() {
-      return src(genGulp.path.sass)
-        .pipe(rename(genGulp.name.css))
-        .pipe(rename(function (path) {
-          return {
-            dirname: path.dirname + "/",
-            basename: path.basename + ".min",
-            extname: ".css",
-          };
-        }))
+        .pipe(dest("dist"));
+    }
+  },
+};
+
+var genScreenGulp = {
+  path: {
+    html: "others/genScreen/src/content.html",
+    js: "others/genScreen/src/script.js",
+    dist: "others/genScreen/dist"
+  },
+  name: {
+    html: "index.html",
+    js: "vendor.js"
+  },
+  export: {
+    js() {
+      return src(genScreenGulp.path.js)
+        .pipe(rename(genScreenGulp.name.js))
         .pipe(sourcemaps.init())
         .pipe(minify({
           mangle: false,
           noSource: true,
           ext: {
-            min: ".min.css"
+            min: ".min.js"
           }
         }))
-        .pipe(sass.sync({
-          "outputStyle": "compressed",
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-          cascade: true
-        }))
         .pipe(sourcemaps.write("./"))
-        .pipe(dest(genGulp.path.dist))
-        .pipe(browserSync.stream());
+        .pipe(dest(genScreenGulp.path.dist));
     },
+    // css() {
+    //   return src(genScreenGulp.path.sass)
+    //     .pipe(rename(genScreenGulp.name.css))
+    //     .pipe(rename(function (path) {
+    //       return {
+    //         dirname: path.dirname + "/",
+    //         basename: path.basename + ".min",
+    //         extname: ".css",
+    //       };
+    //     }))
+    //     .pipe(sourcemaps.init())
+    //     .pipe(minify({
+    //       mangle: false,
+    //       noSource: true,
+    //       ext: {
+    //         min: ".min.css"
+    //       }
+    //     }))
+    //     .pipe(sass.sync({
+    //       "outputStyle": "compressed",
+    //     }).on('error', sass.logError))
+    //     .pipe(autoprefixer({
+    //       cascade: true
+    //     }))
+    //     .pipe(sourcemaps.write("./"))
+    //     .pipe(dest(genScreenGulp.path.dist))
+    //     .pipe(browserSync.stream());
+    // },
     html() {
-      return src(genGulp.path.html)
+      return src(genScreenGulp.path.html)
         .pipe(minifyHTML({
           collapseWhitespace: true,
           removeComments: true,
@@ -154,20 +100,13 @@ var genGulp = {
           minifyCSS: true,
           minifyJS: true,
         }))
-        .pipe(rename(genGulp.name.html))
-        .pipe(dest("./"));
+        .pipe(rename(genScreenGulp.name.html))
+        .pipe(dest("others/genScreen/"));
     },
   },
 };
 
-exports.build = parallel(khp_gulp.export.css, khp_gulp.export.js.standard, genGulp.export.css, genGulp.export.html, genGulp.export.js);
+exports.build = parallel(khplayerGulp.export.js, genScreenGulp.export.js, genScreenGulp.export.html);
 exports.watch = series(exports.build, function () {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  watch([genGulp.path.sass, genGulp.path.js, genGulp.path.html, khp_gulp.path.sass, khp_gulp.path.js, "./demo/index.html"], exports.build, () => {
-    browserSync.reload();
-  });
+  watch([genScreenGulp.path.js, genScreenGulp.path.html, khplayerGulp.path.js, "others/demo/index.html"], {}, exports.build);
 });
