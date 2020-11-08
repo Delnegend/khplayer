e5ece8d9dcbadf96431e2b1d386f3c980c680b04
DELNEGEND <kiennguyen19323@gmail.com>
2020-11-08 23:21:49 +0700

    6.3.1 > 6.3.2

--- dist/khplayer.js
+++ dist/khplayer.js
@@ -1,6 +1,6 @@
 //! ================================================
 //! KHPLayer
-//! Version: 6.3.1
+//! Version: 6.3.2
 //! Homepage/repo: https://khplayer.delnegend.xyz --redirect--> https://github.com/DELNEGEND/khplayer
 //! License: The MIT License (MIT)
 //! ================================================
@@ -125,11 +125,15 @@ khpCtn.querySelector('.plyr>.plyr__controls') && khpCtn.querySelector('.plyr>.pl
 //#endregion
 
 khpCtn.querySelector('.plyr').addEventListener('click', () => {
-if (khpCtn.querySelector('.systemDetectHistory').classList.contains('hideAlert') && !this.plyr[uniqueKey][0].source) {
-khpCtn.querySelector("ul.KHPPlaylistContainer>li:nth-child(1)").click();
+if (playingData) {
+if (khpCtn.querySelector('.systemDetectHistory').classList.contains('hideAlert') && !this.plyr[uniqueKey][0].source)
+khpCtn.querySelector("ul.KHPPlaylistContainer>li:first-child").click();
+} else {
+if (!this.plyr[uniqueKey][0].source) {
+khpCtn.querySelector("ul.KHPPlaylistContainer>li:first-child").click();
+}
 }
 });
-
 },
 tweaks(uniqueKey) {
 let khpCtn = this.id(uniqueKey);
@@ -153,7 +157,7 @@ true
 // Next episode btn
 // <i class="fas fa-step-forward fa-lg"></i>
 this.insertAfter(
-khpCtn.querySelector(".plyr__controls > button:nth-child(1)"),
+khpCtn.querySelector(".plyr__controls > button:first-child"),
 `<button class="plyr__controls__item plyr__control customBtn nextEpBtn" type="button" onclick="KHPlayer.navigateEp(this,'next')">
 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="step-forward" class="svg-inline--fa fa-step-forward fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M384 44v424c0 6.6-5.4 12-12 12h-48c-6.6 0-12-5.4-12-12V291.6l-195.5 181C95.9 489.7 64 475.4 64 448V64c0-27.4 31.9-41.7 52.5-24.6L312 219.3V44c0-6.6 5.4-12 12-12h48c6.6 0 12 5.4 12 12z"></path></svg>
 <span class="plyr__tooltip">${this.configurator.i18n.nextEpisode}</span>
@@ -164,7 +168,7 @@ true
 // Previous episode btn
 // <i class="fas fa-step-backward fa-lg"></i>
 this.insertBefore(
-khpCtn.querySelector(".plyr__controls > button:nth-child(1)"),
+khpCtn.querySelector(".plyr__controls > button:first-child"),
 `<button class="plyr__controls__item plyr__control customBtn prevEpBtn" type="button" onclick="KHPlayer.navigateEp(this,'prev')">
 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="step-backward" class="svg-inline--fa fa-step-backward fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M64 468V44c0-6.6 5.4-12 12-12h48c6.6 0 12 5.4 12 12v176.4l195.5-181C352.1 22.3 384 36.6 384 64v384c0 27.4-31.9 41.7-52.5 24.6L136 292.7V468c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12z"></path></svg>
 <span class="plyr__tooltip">${this.configurator.i18n.previousEpisode}</span>
