7ae3c2b0c409cdecb49454304e38236a099f70ca
Delnegend <contact@delnegend.com>
2021-05-03 02:54:15 +0700

    small adjustments

--- others/genScreen/js/script.js
+++ others/genScreen/js/script.js
@@ -1,19 +1,20 @@
+/* eslint-disable max-len */
 // -------------------------------------
 // Chung
 // -------------------------------------
 // #region
 // Tốc độ hiệu ứng
 const KHPGen = {
-  aniSpeed: "fast",
+  aniSpeed: 'fast',
   minus_icon: `➖`,
   randomString(length = 20) {
     let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let charactersLength = characters.length;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return "_" + result + btoa(new Date().getTime()).replace(/=/g, "");
+    return '_' + result + btoa(new Date().getTime()).replace(/=/g, '');
   },
   aniReplaceHTML(selector, content, duration) {
     $(selector).fadeOut(duration, () => $(selector).html(content).fadeIn(duration));
@@ -21,67 +22,67 @@ const KHPGen = {
   aniAppendTo(takeThis, appendHere, aniSpeed = KHPGen.aniSpeed) {
     $(takeThis)
       .css({
-        "display": "none"
+        'display': 'none',
       })
       .appendTo(appendHere)
       .slideDown(aniSpeed);
   },
   ADD_EPISODE(elem) {
     $(elem)
-      .parent(".addRemoveEpBtn")
-      .parent(".episode_container")
-      .after($("#phantomZoneCtn .episode_container").clone().hide())
+      .parent('.addRemoveEpBtn')
+      .parent('.episode_container')
+      .after($('#phantomZoneCtn .episode_container').clone().hide())
       .next().slideDown(KHPGen.aniSpeed);
   },
   REMOVE_EPISODE(elem) {
-    let a = $(elem.parentNode.parentNode);
+    const a = $(elem.parentNode.parentNode);
     a.slideUp(KHPGen.aniSpeed, () => a.remove());
   },
   ADD_SOURCE(elem) {
     KHPGen.aniAppendTo(
-      $("#temp_zone .singleSrcCtn").clone(),
+      $('#temp_zone .singleSrcCtn').clone(),
       elem.parentNode.parentNode,
       KHPGen.aniSpeed);
   },
   REMOVE_SOURCE(elem) {
-    let a = $(elem.parentNode);
+    const a = $(elem.parentNode);
     a.slideUp(KHPGen.aniSpeed, () => a.remove());
   },
   ADD_CAPTIONS(elem) {
     KHPGen.aniAppendTo(
-      $("#temp_zone .singleCapCtn").clone(),
+      $('#temp_zone .singleCapCtn').clone(),
       $(elem.parentNode.parentNode),
-      KHPGen.aniSpeed
+      KHPGen.aniSpeed,
     );
   },
   REMOVE_CAPTIONS(elem) {
     $(elem.parentNode).slideUp(KHPGen.aniSpeed, () => $(elem.parentNode).remove());
   },
   SELECT_TYPE(elem) {
-    let a = $(elem)
-      .parent(".dropdown-menu")
-      .parent("#typeSelectorContainer")
-      .find("#typeSelectorBtn"),
-      b = $(elem).html();
+    const a = $(elem)
+      .parent('.dropdown-menu')
+      .parent('#typeSelectorContainer')
+      .find('#typeSelectorBtn');
+    const b = $(elem).html();
     KHPGen.aniReplaceHTML(a, b, KHPGen.aniSpeed);
-    a.attr("selected-type", $(elem).attr("type-data"));
+    a.attr('selected-type', $(elem).attr('type-data'));
   },
   SELECT_RES(elem) {
-    let a =
+    const a =
       $(elem)
-      .parent(".dropdown-menu")
-      .parent("#qualitySelectorContainer")
-      .find("#qualitySelectorBtn"),
-      b = $(elem).html();
+        .parent('.dropdown-menu')
+        .parent('#qualitySelectorContainer')
+        .find('#qualitySelectorBtn');
+    const b = $(elem).html();
     KHPGen.aniReplaceHTML(a, b, KHPGen.aniSpeed);
-    a.attr("selected-res", $(elem).attr("res-data"));
+    a.attr('selected-res', $(elem).attr('res-data'));
   },
   saveAsJSON(string) {
-    let text = string,
-      filename = UNIQUE_ID,
-      blob = new Blob([text], {
-        type: "application/json"
-      });
+    const text = string;
+    const filename = UNIQUE_ID;
+    const blob = new Blob([text], {
+      type: 'application/json',
+    });
     saveAs(blob, filename);
   },
   downloadData() {
@@ -90,63 +91,65 @@ const KHPGen = {
   completeResult(elem) {
     $('#masterPosterContainer, .episode_container').slideUp(KHPGen.aniSpeed);
     $('#addOrCreateNewCtn').slideDown(KHPGen.aniSpeed);
-    elem.querySelectorAll('i-18n')[0].setAttribute("hidden", "");
-    elem.querySelectorAll('i-18n')[1].removeAttribute("hidden");
-    $(elem).attr('onclick', "KHPGen.editResult(this)");
+    elem.querySelectorAll('i-18n')[0].setAttribute('hidden', '');
+    elem.querySelectorAll('i-18n')[1].removeAttribute('hidden');
+    $(elem).attr('onclick', 'KHPGen.editResult(this)');
   },
   editResult(elem) {
     $('#addOrCreateNewCtn, #createNewPPCtn, #existedPPCtn').slideUp(KHPGen.aniSpeed);
     $('#masterPosterContainer, .episode_container').slideDown(KHPGen.aniSpeed);
-    elem.querySelectorAll('i-18n')[0].removeAttribute("hidden");
-    elem.querySelectorAll('i-18n')[1].setAttribute("hidden", "");
-    $(elem).attr('onclick', "KHPGen.completeResult(this)");
+    elem.querySelectorAll('i-18n')[0].removeAttribute('hidden');
+    elem.querySelectorAll('i-18n')[1].setAttribute('hidden', '');
+    $(elem).attr('onclick', 'KHPGen.completeResult(this)');
   },
   generateCodeFromUserInput(addPosterAndPlayingIndicator = true) {
     // Ẩn khung export
-    $("#outputPanel").slideUp(KHPGen.aniSpeed);
+    $('#outputPanel').slideUp(KHPGen.aniSpeed);
 
     // Array chứa data thô toàn bộ input từ người dùng
-    let RAW_DATA = [];
+    const RAW_DATA = [];
 
     // Xử lý từng episode
-    for (let value of document.querySelectorAll("#userInteractCtn .episode_container")) {
+    for (const value of document.querySelectorAll('#userInteractCtn .episode_container')) {
       if (value.querySelector('.title').value) {
         // Object tạm thời để đẩy dần dữ liệu vào, cuối cùng đẩy vào RAW_DATA
-        let processingEpElem = {};
+        const processingEpElem = {};
 
-        processingEpElem.type = "video";
-        processingEpElem.title = value.querySelector(".title").value;
+        processingEpElem.type = 'video';
+        processingEpElem.title = value.querySelector('.title').value;
         processingEpElem.sources = [];
         processingEpElem.tracks = [];
-        
+
         // Sprite preview thumbnail
-        let preThumbSrc = value.querySelector(".previewThumbnail").value;
-        if (preThumbSrc) processingEpElem.previewThumbnails = {
-          src: preThumbSrc
-        };
+        const preThumbSrc = value.querySelector('.previewThumbnail').value;
+        if (preThumbSrc) {
+          processingEpElem.previewThumbnails = {
+            src: preThumbSrc,
+          };
+        }
 
         // Xử lý từng source
-        for (let source of value.querySelectorAll("#ALL_SRCS_CTN .singleSrcCtn")) {
-          let currentMediaSrc = {};
+        for (const source of value.querySelectorAll('#ALL_SRCS_CTN .singleSrcCtn')) {
+          const currentMediaSrc = {};
 
           // URL + type
-          currentMediaSrc.src = source.querySelector(".mediaSrc").value;
-          currentMediaSrc.type = source.querySelector("#typeSelectorBtn").getAttribute("selected-type");
+          currentMediaSrc.src = source.querySelector('.mediaSrc').value;
+          currentMediaSrc.type = source.querySelector('#typeSelectorBtn').getAttribute('selected-type');
 
           // Quality
-          currentMediaSrc.size = parseInt(source.querySelector("#qualitySelectorBtn").getAttribute("selected-res"));
+          currentMediaSrc.size = parseInt(source.querySelector('#qualitySelectorBtn').getAttribute('selected-res'));
 
           // Push lên processingEpElem.sources
           processingEpElem.sources.push(currentMediaSrc);
         }
         // Xử lý từng phụ đề
-        for (let caption of value.querySelectorAll("#ALL_CAPS_CTN .singleCapCtn")) {
-          if (caption.querySelector(".captionFullName").value) {
-            let processingCaption = {};
-            processingCaption.kind = "subtitles";
-            processingCaption.label = caption.querySelector(".captionFullName").value;
-            processingCaption.srclang = caption.querySelector(".captionShortName").value;
-            processingCaption.src = caption.querySelector(".capionSrc").value;
+        for (const caption of value.querySelectorAll('#ALL_CAPS_CTN .singleCapCtn')) {
+          if (caption.querySelector('.captionFullName').value) {
+            const processingCaption = {};
+            processingCaption.kind = 'subtitles';
+            processingCaption.label = caption.querySelector('.captionFullName').value;
+            processingCaption.srclang = caption.querySelector('.captionShortName').value;
+            processingCaption.src = caption.querySelector('.capionSrc').value;
             processingEpElem.tracks.push(processingCaption);
           }
         }
@@ -158,103 +161,101 @@ const KHPGen = {
 
     // Đấy url poster tổng cũng như ID lên data
     if (addPosterAndPlayingIndicator) {
-      RAW_DATA.push($("#masterPosterInput").val() || "");
+      RAW_DATA.push($('#masterPosterInput').val() || '');
       RAW_DATA.push(UNIQUE_ID);
     }
     return JSON.stringify(RAW_DATA);
   },
-  copy2clip: new ClipboardJS(".copy2clip"),
-  messageBox(message = "Thông báo!", duration = 1000) {
+  copy2clip: new ClipboardJS('.copy2clip'),
+  messageBox(message = 'Thông báo!', duration = 1000) {
     // Type: https://getbootstrap.com/docs/4.0/components/alerts/
 
     // Tạo 1 id tạm thời cho container thông báo, mỗi 1 noti là 1 id riêng, tránh trùng lặp với noti trước nếu 2 cái overlap nhau
-    let tempID = KHPGen.randomString();
+    const tempID = KHPGen.randomString();
     // Tạo container chứa messageBox nếu chưa có
     if ($('.alertCtn')[0] === void 0) {
-      $("body").append(`<div class="alertCtn"><div class="innerAlertCtn"></div></div>`);
+      $('body').append(`<div class="alertCtn"><div class="innerAlertCtn"></div></div>`);
     }
     $('.innerAlertCtn').append(`<div class="alert" id="${tempID}" role="alert" display="none">${message}</div>`);
-    $("#" + tempID).slideDown("normal");
+    $('#' + tempID).slideDown('normal');
     setTimeout(() => {
-      $("#" + tempID).slideUp("normal", () => $("#" + tempID).remove());
+      $('#' + tempID).slideUp('normal', () => $('#' + tempID).remove());
     }, duration);
   },
 };
-var UNIQUE_ID = "khplayer" + KHPGen.randomString();
+const UNIQUE_ID = 'khplayer' + KHPGen.randomString();
 
-$("#qualitySelectorContainer .dropdown-item").attr("onclick", "KHPGen.SELECT_RES(this)");
-$("#typeSelectorContainer .dropdown-item").attr("onclick", "KHPGen.SELECT_TYPE(this)");
+$('#qualitySelectorContainer .dropdown-item').attr('onclick', 'KHPGen.SELECT_RES(this)');
+$('#typeSelectorContainer .dropdown-item').attr('onclick', 'KHPGen.SELECT_TYPE(this)');
 
 // Clone toàn bộ template vào mainframe
-$("#phantomZoneCtn .episode_container")
+$('#phantomZoneCtn .episode_container')
   .clone()
-  .appendTo("#userInteractCtn")
+  .appendTo('#userInteractCtn')
   .show();
 
 // Clone .singleSrcCtn từ template vào #temp_zone để xử lý
-$("#phantomZoneCtn #ALL_SRCS_CTN .singleSrcCtn")
+$('#phantomZoneCtn #ALL_SRCS_CTN .singleSrcCtn')
   .clone()
-  .appendTo("#phantomZoneCtn #temp_zone");
+  .appendTo('#phantomZoneCtn #temp_zone');
 
 // addSource -> removeSource trong #temp_zone
-$("#temp_zone #addSourceBtn")
-  .attr("onclick", "KHPGen.REMOVE_SOURCE(this)")
-  .removeAttr("id")
+$('#temp_zone #addSourceBtn')
+  .attr('onclick', 'KHPGen.REMOVE_SOURCE(this)')
+  .removeAttr('id')
   .html(KHPGen.minus_icon);
 
 // Clone .singleCapCtn từ template vào #temp_zone để xử lý
-$("#phantomZoneCtn #ALL_CAPS_CTN .singleCapCtn")
+$('#phantomZoneCtn #ALL_CAPS_CTN .singleCapCtn')
   .clone()
-  .appendTo("#phantomZoneCtn #temp_zone");
+  .appendTo('#phantomZoneCtn #temp_zone');
 
 // addCaps -> removeCaps trong .singleCapCtn trong #temp_zone
-$("#temp_zone #addCaptionBtn")
-  .attr("onclick", "KHPGen.REMOVE_CAPTIONS(this)")
-  .removeAttr("id")
+$('#temp_zone #addCaptionBtn')
+  .attr('onclick', 'KHPGen.REMOVE_CAPTIONS(this)')
+  .removeAttr('id')
   .html(KHPGen.minus_icon);
 
 // Clone nút add từ template vào #temp_zone để xử lý
-$("#phantomZoneCtn #addEpBtn").clone().appendTo("#phantomZoneCtn .addRemoveEpBtn");
+$('#phantomZoneCtn #addEpBtn').clone().appendTo('#phantomZoneCtn .addRemoveEpBtn');
 
 // addEp -> removeEp trong #temp_zone
-$("#phantomZoneCtn #addEpBtn:nth-child(2)")
-  .removeAttr("id")
-  .attr("onclick", "KHPGen.REMOVE_EPISODE(this)")
+$('#phantomZoneCtn #addEpBtn:nth-child(2)')
+  .removeAttr('id')
+  .attr('onclick', 'KHPGen.REMOVE_EPISODE(this)')
   .html(KHPGen.minus_icon);
 
-$(".editResult").hide();
-$("#downloadJSON").on("click", () => KHPGen.saveAsJSON(KHPGen.generateCodeFromUserInput()));
+$('.editResult').hide();
+$('#downloadJSON').on('click', () => KHPGen.saveAsJSON(KHPGen.generateCodeFromUserInput()));
 
-$("#createNewPPActionBtn").on("click", () => {
-  $("#createNewPPCode>code").html(`&#x3C;khplayer-container data=&#x22;${UNIQUE_ID}.json&#x22;&#x3E;&#x3C;/khplayer-container&#x3E;`);
-  $("#existedPPCtn").slideUp(KHPGen.aniSpeed);
-  $("#createNewPPCtn").slideDown(KHPGen.aniSpeed);
+$('#createNewPPActionBtn').on('click', () => {
+  $('#createNewPPCode>code').html(`&#x3C;khplayer-container data=&#x22;${UNIQUE_ID}.json&#x22;&#x3E;&#x3C;/khplayer-container&#x3E;`);
+  $('#existedPPCtn').slideUp(KHPGen.aniSpeed);
+  $('#createNewPPCtn').slideDown(KHPGen.aniSpeed);
 });
-$("#existedPPActionBtn").on("click", () => {
+$('#existedPPActionBtn').on('click', () => {
   let newData = KHPGen.generateCodeFromUserInput(false);
   newData = newData.slice(1, newData.length - 3);
-  $("#existedPPCtnCode>code").html(newData);
-  $("#createNewPPCtn").slideUp(KHPGen.aniSpeed);
-  $("#existedPPCtn").slideDown(KHPGen.aniSpeed);
+  $('#existedPPCtnCode>code').html(newData);
+  $('#createNewPPCtn').slideUp(KHPGen.aniSpeed);
+  $('#existedPPCtn').slideDown(KHPGen.aniSpeed);
 });
-KHPGen.copy2clip.on("success", e => {
+KHPGen.copy2clip.on('success', (e) => {
   e.clearSelection();
-  KHPGen.messageBox("Đã copy vào clipboard!");
+  KHPGen.messageBox('Đã copy vào clipboard!');
 });
-
-
 
 
 // i18n
-document.addEventListener("DOMContentLoaded", () => {
-  let params = new URLSearchParams(window.location.search),
-    language = params.get("lang");
+document.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.search);
+  const language = params.get('lang');
 
-  if (language == "en") {
-    for (let elem of document.querySelectorAll("i-18n")) elem.innerHTML = elem.getAttribute("en");
-    for (let elem of document.querySelectorAll('*[ph-en]')) elem.setAttribute("placeholder", elem.getAttribute("ph-en"));
+  if (language == 'en') {
+    for (const elem of document.querySelectorAll('i-18n')) elem.innerHTML = elem.getAttribute('en');
+    for (const elem of document.querySelectorAll('*[ph-en]')) elem.setAttribute('placeholder', elem.getAttribute('ph-en'));
   } else {
-    for (let elem of document.querySelectorAll("i-18n")) elem.innerHTML = elem.getAttribute("vi");
-    for (let elem of document.querySelectorAll('*[ph-vi]')) elem.setAttribute("placeholder", elem.getAttribute("ph-vi"));
+    for (const elem of document.querySelectorAll('i-18n')) elem.innerHTML = elem.getAttribute('vi');
+    for (const elem of document.querySelectorAll('*[ph-vi]')) elem.setAttribute('placeholder', elem.getAttribute('ph-vi'));
   }
 });
--- others/genSpriteThumb/index.js
+++ others/genSpriteThumb/index.js
@@ -1,66 +1,67 @@
+/* eslint-disable max-len */
 // jshint esversion: 8
 const d = document;
 const genCode = {
-    changeResult() {
-        let
-            parentSelect = '.container ',
-            filename = $(parentSelect + '#filename').val() || $(parentSelect + '#filename').attr('placeholder'),
-            fps = $(parentSelect + '#fps').val() || $(parentSelect + '#fps').attr('placeholder'),
-            hour = $(parentSelect + '#hour').val() || $(parentSelect + '#hour').attr('placeholder'),
-            minute = $(parentSelect + '#minute').val() || $(parentSelect + '#minute').attr('placeholder'),
-            second = $(parentSelect + '#second').val() || $(parentSelect + '#second').attr('placeholder'),
-            width = $(parentSelect + '#width').val() || $(parentSelect + '#width').attr('placeholder'),
-            height = $(parentSelect + '#height').val() || $(parentSelect + '#height').attr('placeholder'),
-            interval = $(parentSelect + '#interval').val() || $(parentSelect + '#interval').attr('placeholder'),
-            thumbUrl = $(parentSelect + "#thumbUrl").val() || $(parentSelect + "#thumbUrl").attr('placeholder');
+  changeResult() {
+    const
+      parentSelect = '.container ';
+    const filename = $(parentSelect + '#filename').val() || $(parentSelect + '#filename').attr('placeholder');
+    const fps = $(parentSelect + '#fps').val() || $(parentSelect + '#fps').attr('placeholder');
+    const hour = $(parentSelect + '#hour').val() || $(parentSelect + '#hour').attr('placeholder');
+    const minute = $(parentSelect + '#minute').val() || $(parentSelect + '#minute').attr('placeholder');
+    const second = $(parentSelect + '#second').val() || $(parentSelect + '#second').attr('placeholder');
+    const width = $(parentSelect + '#width').val() || $(parentSelect + '#width').attr('placeholder');
+    const height = $(parentSelect + '#height').val() || $(parentSelect + '#height').attr('placeholder');
+    const interval = $(parentSelect + '#interval').val() || $(parentSelect + '#interval').attr('placeholder');
+    const thumbUrl = $(parentSelect + '#thumbUrl').val() || $(parentSelect + '#thumbUrl').attr('placeholder');
 
-        let totalVidDur = hour * 3600 + minute * 60 + second,
-            grid = Math.round(Math.sqrt(totalVidDur / interval)) + 1;
+    const totalVidDur = hour * 3600 + minute * 60 + second;
+    const grid = Math.round(Math.sqrt(totalVidDur / interval)) + 1;
 
-        let ffmpegCmd = `ffmpeg -i ${filename} -filter_complex "select='not(mod(n,${fps*interval}))',scale=${width}:${height},tile=${grid}x${grid}" -frames:v 1 -qscale:v 3 -an ${filename}_mosaic.jpg`;
-        let nodeCmd = `node create.js ${totalVidDur} '${thumbUrl}' '${filename}_mosaic.vtt' ${interval} ${width} ${height} ${grid}`
-        $('#ffmpegCmd').html(ffmpegCmd);
-        $('#nodeCmd').html(nodeCmd);
-    }
+    const ffmpegCmd = `ffmpeg -i ${filename} -filter_complex "select='not(mod(n,${fps * interval}))',scale=${width}:${height},tile=${grid}x${grid}" -frames:v 1 -qscale:v 3 -an ${filename}_mosaic.jpg`;
+    const nodeCmd = `node create.js ${totalVidDur} '${thumbUrl}' '${filename}_mosaic.vtt' ${interval} ${width} ${height} ${grid}`;
+    $('#ffmpegCmd').html(ffmpegCmd);
+    $('#nodeCmd').html(nodeCmd);
+  },
 };
-document.addEventListener("DOMContentLoaded", () => {
-    let params = new URLSearchParams(window.location.search),
-        language = params.get("lang");
-    for (let elem of d.querySelectorAll('copy-button')) {
-        elem.innerHTML =
-        /* html */`
+document.addEventListener('DOMContentLoaded', () => {
+  const params = new URLSearchParams(window.location.search);
+  const language = params.get('lang');
+  for (const elem of d.querySelectorAll('copy-button')) {
+    elem.innerHTML =
+      /* html */`
         <button type="button" class="btn btn-success" data-clipboard-target='${elem.getAttribute('data-target')}'>
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-clipboard-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
                 <path fill-rule="evenodd" d="M9.5 1h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3zM8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/>
             </svg>
         </button>`
-        ;
-    }
-    for (let elem of d.querySelectorAll('input-field')) {
-        let
-            title = ((language == "en") && elem.getAttribute('t-en')) || elem.getAttribute('t-vi') || "",
-            placeholder = elem.getAttribute('placeholder') || "",
-            id = elem.getAttribute('_id') || "",
-            type = elem.getAttribute('type') || "";
-        elem.innerHTML = `
+    ;
+  }
+  for (const elem of d.querySelectorAll('input-field')) {
+    const
+      title = ((language == 'en') && elem.getAttribute('t-en')) || elem.getAttribute('t-vi') || '';
+    const placeholder = elem.getAttribute('placeholder') || '';
+    const id = elem.getAttribute('_id') || '';
+    const type = elem.getAttribute('type') || '';
+    elem.innerHTML = `
     <div class="input-group mb-3">
       <div class="input-group-prepend">
         <span class="input-group-text">${title}</span>
       </div>
       <input oninput="genCode.changeResult()" type="${type}" class="form-control" placeholder="${placeholder}" aria-label="${placeholder}" id="${id}">
     </div>`;
-    }
+  }
 
-    genCode.changeResult();
+  genCode.changeResult();
 
-    $('copy-button').on('click', () => new ClipboardJS('copy-button .btn'));
+  $('copy-button').on('click', () => new ClipboardJS('copy-button .btn'));
 
-    if (language == "en") {
-        for (let elem of document.querySelectorAll("i-18n")) elem.innerHTML = elem.getAttribute("en");
-        // for (let elem of document.querySelectorAll('*[t-en]')) elem.setAttribute("title", elem.getAttribute("ph-en"));
-    } else {
-        for (let elem of document.querySelectorAll("i-18n")) elem.innerHTML = elem.getAttribute("vi");
-        // for (let elem of document.querySelectorAll('*[t-vi]')) elem.setAttribute("title", elem.getAttribute("ph-vi"));
-    }
+  if (language == 'en') {
+    for (const elem of document.querySelectorAll('i-18n')) elem.innerHTML = elem.getAttribute('en');
+    // for (let elem of document.querySelectorAll('*[t-en]')) elem.setAttribute("title", elem.getAttribute("ph-en"));
+  } else {
+    for (const elem of document.querySelectorAll('i-18n')) elem.innerHTML = elem.getAttribute('vi');
+    // for (let elem of document.querySelectorAll('*[t-vi]')) elem.setAttribute("title", elem.getAttribute("ph-vi"));
+  }
 });
